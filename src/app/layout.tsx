83995547f2a0fffd181967066ba83e47ead71e0d
Robert Mill <81169127+RobertVMill@users.noreply.github.com>
2025-01-12 12:07:34 -0500

    feat: Restore dark mode theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,12 +10,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, viewport-fit=cover" />
       </head>
       <body>
-        <Theme>
+        <Theme appearance="dark" accentColor="blue" radius="large">
           <main style={{ 
             maxWidth: '100vw', 
             overflowX: 'hidden',
