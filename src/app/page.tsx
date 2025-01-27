e6dd7aac1cc2ac7779eab54416fa4cdbe8f73bfa
Robert Mill <81169127+RobertVMill@users.noreply.github.com>
2025-01-27 16:06:26 -0500

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
           }}>
             <Flex direction="column" gap="3">
               <Text size="3" style={{ lineHeight: '1.6' }}>
-                Beart, the people you spend your time with make you. Attract them through inspirational pursuits Hyrox & AI. Find people who inspire you and give them your all. Nic, Michael, Jeff B, Kree, Monique. David. Mom. Dad. Tomi.
+                Beart, the people you spend your time with make you. Attract them through inspirational pursuits Hyrox & AI. Find people who inspire you and give them your all. Nic, Michael P, Michael L, Jeff B, Kree, Monique. David. Mom. Dad. Tomi.
               </Text>
               
               <Text size="3" style={{ lineHeight: '1.6' }}>
