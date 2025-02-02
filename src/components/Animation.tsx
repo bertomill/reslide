290ecf239680c36550b4a8042c5171ae2108d7b6
Robert Mill <81169127+RobertVMill@users.noreply.github.com>
2025-02-02 14:56:06 -0500

    new

--- src/components/Animation.tsx
+++ src/components/Animation.tsx
@@ -1,16 +1,60 @@
 'use client';
 
 import Lottie from 'lottie-react';
-import animationData from './animation.json';
+
+// We'll need to create or import an actual animation JSON file
+const defaultAnimation = {
+  v: "5.5.7",
+  fr: 60,
+  ip: 0,
+  op: 180,
+  w: 512,
+  h: 512,
+  nm: "Loading",
+  ddd: 0,
+  assets: [],
+  layers: [
+    // Basic loading animation
+    {
+      ddd: 0,
+      ind: 1,
+      ty: 4,
+      nm: "Shape Layer",
+      sr: 1,
+      ks: {
+        o: { a: 0, k: 100 },
+        r: {
+          a: 1,
+          k: [
+            { t: 0, s: [0], h: 0 },
+            { t: 180, s: [360], h: 0 }
+          ]
+        },
+        p: { a: 0, k: [256, 256, 0] },
+        a: { a: 0, k: [0, 0, 0] },
+        s: { a: 0, k: [100, 100, 100] }
+      },
+      ao: 0,
+      shapes: [{
+        ty: "el",
+        p: { a: 0, k: [0, 0] },
+        s: { a: 0, k: [100, 100] }
+      }],
+      ip: 0,
+      op: 180,
+      st: 0
+    }
+  ]
+};
 
 export default function Animation() {
   return (
     <div style={{ 
-      width: '300px',  // or whatever size you want
+      width: '300px',
       margin: '0 auto'
     }}>
       <Lottie 
-        animationData={animationData}
+        animationData={defaultAnimation}
         loop={true}
         autoplay={true}
       />
